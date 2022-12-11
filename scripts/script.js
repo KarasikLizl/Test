67487870134d8f2f2ae5a92988f349813953c847
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2022-12-11 20:42:42 +0300

    добавление анимации

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const page = document.querySelector(".page");
 const menu = page.querySelector(".header__item_main");
 const menuList = page.querySelector(".header__menu");
+const input = page.querySelector(".form__input");
 
 const maxWidth = 767;
 
@@ -20,3 +21,8 @@ function showMenu(event) {
 }
 
 menu.addEventListener("click", showMenu);
+
+input.addEventListener("input", function(e){
+  let inputValue = input.value.replace(/[!,@,#,$,%,^,&,*,(,)]/g, '');
+  input.value = inputValue;
+});
